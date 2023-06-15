0f0ed78c9e674f2c0a25933505688d1817a322cb
sarath-hover <sarathhover47@gmail.com>
2023-06-15 19:15:45 +0530

    commit on jun15th2023 finished menu section

--- script.js
+++ script.js
@@ -2,17 +2,31 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+
 }
 
-let cartitem = document.querySelector('.cart-items-container');
+let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () =>{
-    cartitem.classList.toggle('active');
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
 }
+ 
+    //  SEARCH FORM BTN SHOWS SOME ISSUES   
 
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
